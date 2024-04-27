c9863c60c2cd8854f9722439345060533bd3ecb9
Wei <>
2024-04-27 18:17:49 +0800

    postgres

--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	go.uber.org/zap v1.27.0
 	golang.org/x/crypto v0.22.0
 	gorm.io/driver/mysql v1.5.6
+	gorm.io/driver/postgres v1.5.7
 	gorm.io/gorm v1.25.10
 	gorm.io/plugin/dbresolver v1.5.1
 )
@@ -22,6 +23,9 @@ require (
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.4.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
--- pkg/postgres/postgres.go
+++ pkg/postgres/postgres.go
@@ -3,7 +3,7 @@ package mysql
 import (
 	"errors"
 	"fmt"
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"gorm.io/plugin/dbresolver"
 	"sync"
@@ -44,9 +44,9 @@ func LoadPgDb() *DbInfo {
 
 func (info *DbInfo) InitConnect() {
 	dbConf := info.DbConfig
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True", dbConf.User, dbConf.Password, dbConf.Host, dbConf.Port, dbConf.Database)
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai", dbConf.Host, dbConf.User, dbConf.Password, dbConf.Database, dbConf.Port)
 
-	if db, e := gorm.Open(mysql.Open(dsn), &gorm.Config{}); e != nil {
+	if db, e := gorm.Open(postgres.Open(dsn), &gorm.Config{}); e != nil {
 		info.Conn = nil
 	} else {
 		if dbConf.Idle == 0 {
